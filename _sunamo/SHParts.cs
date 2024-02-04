cf4236689922c57524ed68e84d31b515f6976322
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-02-04 12:09:17 +0100

    .

--- _sunamo/SHParts.cs
+++ _sunamo/SHParts.cs
@@ -5,20 +5,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace SunamoUri._sunamo
-{
-    internal class SHParts
-    {
-        internal static string RemoveAfterFirst(string t, string ch)
-        {
-            int dex = t.IndexOf(ch);
-            if (dex == -1 || dex == t.Length - 1)
-            {
-                return t;
-            }
+namespace SunamoUri._sunamo;
 
-            string vr = t.Remove(dex);
-            return vr;
+internal class SHParts
+{
+    internal static string RemoveAfterFirst(string t, string ch)
+    {
+        int dex = t.IndexOf(ch);
+        if (dex == -1 || dex == t.Length - 1)
+        {
+            return t;
         }
+
+        string vr = t.Remove(dex);
+        return vr;
     }
 }
